04d30c5392a7c71b5c6b4f3ffb2bfe31ed43c641
Your Namesifer <olegajun@gmail.com>
2022-10-19 22:01:32 +0300

    asdf

--- game.py
+++ game.py
@@ -10,10 +10,15 @@ pygame.display.set_caption('Tiny Runner Pablo')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/unlearne.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/Ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/Ground.png').convert()
 text_surface = test_font.render('Tiny Runner Pablo', False, 'Black')
-snail_surface = pygame.image.load('graphics/enemy/enemy1.png')
+
+snail_surface = pygame.image.load('graphics/enemy/enemy1.png').convert_alpha()
+snail_x_pos = 600
+
+player_surf = pygame.image.load(
+    'graphics/player/player_walk1.png').convert_alpha()
 
 while True:
     for event in pygame.event.get():
@@ -24,7 +29,14 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 280))
     screen.blit(text_surface, (200, 50))
-    screen.blit(snail_surface, (600, 240))
+    screen
+
+    snail_x_pos -= 4
+    if snail_x_pos < -35:
+        snail_x_pos = 835
+
+    screen.blit(snail_surface, (snail_x_pos, 240))
+    screen.blilt(player_surf, (80, 240))
 
     # draw all elements
     # update everything

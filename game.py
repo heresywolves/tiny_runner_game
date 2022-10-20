c8a627b5b30553839a30c570c0044f516ec3cc58
Your Namesifer <olegajun@gmail.com>
2022-10-20 17:12:02 +0300

    bg for text

--- game.py
+++ game.py
@@ -10,10 +10,11 @@ pygame.display.set_caption('Tiny Runner Pablo')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/unlearne.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png').convert()
-ground_surface = pygame.image.load('graphics/Ground.png').convert()
-text_surface = test_font.render('Tiny Runner Pablo', False, 'Black')
-text_rect = text_surface.get_rect(midtop=(400, 50))
+sky_surf = pygame.image.load('graphics/Sky.png').convert()
+ground_surf = pygame.image.load('graphics/Ground.png').convert()
+
+score_surf = test_font.render('Tiny Runner Pablo', False, 'Black')
+score_rect = score_surf.get_rect(midtop=(400, 50))
 
 enemy_surf = pygame.image.load('graphics/enemy/enemy1.png').convert_alpha()
 enemy_rect = enemy_surf.get_rect(bottomleft=(600, 310))
@@ -31,9 +32,11 @@ while True:
         #     if player_rect.collidepoint(event.pos):
         #         print('collision')
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 280))
-    screen.blit(text_surface, text_rect)
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 280))
+    pygame.draw.rect(screen, 'Pink', score_rect)
+    pygame.draw.rect(screen, 'Pink', score_rect, 10)
+    screen.blit(score_surf, score_rect)
 
     enemy_rect.left -= 4
     if enemy_rect.left < -35:
